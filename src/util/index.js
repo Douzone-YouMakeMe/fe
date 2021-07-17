c840c4948db01129f58d717e3d88a7b153f78e12
gyunhwan <>
2021-07-17 16:12:45 +0900

    final

--- src/util/index.js
+++ src/util/index.js
@@ -1,6 +1,6 @@
-export const serverUrl = 'http://15.164.83.176:8080';
+export const serverUrl = 'https://youmakeme.click:7070';
 // export const serverUrl = 'http://192.168.0.197:8080';
-export const socketUrl = 'ws://15.164.83.176:8080';
+export const socketUrl = 'wss://youmakeme.click:7070';
 // export const serverUrl = 'http://localhost:8080';
 export const s3Bucket =
   'https://s3.ap-northeast-2.amazonaws.com/douzoneimagebox/umakeme/';
